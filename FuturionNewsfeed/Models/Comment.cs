0c9edb3d26aba7b0b78b7939aef2e0296ff77253
Istvan <gravitydrummer@gmail.com>
2019-03-12 13:00:44 +0100

    Minor format changes in model classes.

--- FuturionNewsfeed/Models/Comment.cs
+++ FuturionNewsfeed/Models/Comment.cs
@@ -9,11 +9,15 @@ namespace FuturionNewsfeed.Models
     public class Comment
     {
         public int Id { get; set; }
+
         [Display(Name = "Megjegyzés szövege")]
         public string Text { get; set; }
+
         [DataType(DataType.DateTime)]
         public DateTime CreationDateTime { get; set; }
+
         public string CreatorUsername { get; set; }
+
         public NewsItem News { get; set; }
     }
 }
--- FuturionNewsfeed/Models/NewsItem.cs
+++ FuturionNewsfeed/Models/NewsItem.cs
@@ -10,19 +10,26 @@ namespace FuturionNewsfeed.Models
     public class NewsItem
     {
         public int Id { get; set; }
+
         [Required]
         [DisplayName("Cím")]
         public string Title { get; set; }
+
         [DisplayName("Tartalom")]
         public string Text { get; set; }
+
         [DataType(DataType.DateTime)]
         [DisplayName("Létrehozás dátuma")]
         public DateTime CreationDateTime { get; set; }
+
         [DisplayName("Létrehozó felhasználóneve")]
         public string CreatorUsername { get; set; }
+
         [DisplayName("Megjegyzések")]
         public ICollection<Comment> Comments { get; set; }
+
         public int NumberOfComments => (Comments != null) ? Comments.Count : 0;
+
         public string ShortenedText => (Text.Length <= 80) ? Text : Text.Substring(0, 80);
     }
 }
